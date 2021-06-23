67e26e7964b37de57bcd58bd7227b21fd3d34050
jon <jonathanyusf@gmail.com>
2021-06-22 19:41:43 -0700

    Add playerSprite

--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@ canvas.width = 800;
 canvas.height = 600;
 
 const boss = {
-  x: 0,
-  y: 0,
+  x: 100,
+  y: 150,
   width: 240,
   height: 256,
-  frameX: 0,
-  frameY: 0,
+  hp: 3000,
+  mp: 9999
+};
+
+const player = {
+  x: 700,
+  y: 200,
+  width: 32,
+  height: 48,
+  hp: 999,
+  mp: 999
 };
 
 const bossSprite = new Image();
@@ -20,10 +29,14 @@ bossSprite.src = "boss.gif"
 const background = new Image();
 background.src = "background.gif";
 
+const playerSprite = new Image();
+playerSprite.src = "cecil.gif"
+
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(bossSprite, 100, 100, boss.width, boss.height);
+  ctx.drawImage(bossSprite, boss.x, boss.y, boss.width, boss.height);
+  ctx.drawImage(playerSprite, player.x, player.y, player.width, player.height);
   requestAnimationFrame(animate);
 }
 
