d7a112a0719dcf27eda0596bd896ffddd7d64aa4
jon <jonathanyusf@gmail.com>
2021-06-25 09:37:30 -0700

    Add boss attack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import "./styles/index.scss";
-import Player from "./utils/player"
-import Boss from "./utils/boss"
-window.attack = attack
-
+import Player from "./utils/player";
+import Boss from "./utils/boss";
+window.attack = attack;
 
 // const battleMenu = document.getElementById("battleMenu")
 // const attackBtnContainer = document.getElementById("attackBtnContainer")
@@ -17,16 +16,16 @@ const boss = new Boss();
 const player = new Player();
 
 const bossSprite = new Image();
-bossSprite.src = "./src/utils/assets/boss.gif"
+bossSprite.src = "./src/utils/assets/boss.gif";
 
 const background = new Image();
 background.src = "./src/utils/assets/background.gif";
 
 const playerSprite = new Image();
-playerSprite.src = "./src/utils/assets/cecil.gif"
+playerSprite.src = "./src/utils/assets/cecil.gif";
 
 const attackAudio = new Audio();
-attackAudio.src = "./src/utils/assets/attack.mp3"
+attackAudio.src = "./src/utils/assets/attack.mp3";
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -38,30 +37,31 @@ function animate() {
 
 animate();
 
-
 function resetPlayer() {
   player.x = 700;
 }
-//attack 
+//attack
 function attack() {
   player.x = 600;
   attackAudio.play();
-  let hitChance = Math.round(Math.random() *10);
+  let hitChance = Math.round(Math.random() * 10);
   if (hitChance >= 3) {
-    let dmg = Math.round(Math.random()*10) + player.dmg;
+    let dmg = Math.round(Math.random() * 10) + player.dmg;
     //append div?
     boss.hp -= dmg;
-    let text = document.createElement('div')
-    let body = document.querySelector('body')
-    text.classList.add("dmg-text")
-    text.append(`You did ${dmg} dmg to the boss, the boss now has ${boss.hp} hp! `)
+    let text = document.createElement("div");
+    let body = document.querySelector("body");
+    text.classList.add("dmg-text");
+    text.append(
+      `You dealt ${dmg} dmg to the boss, the boss now has ${boss.hp} hp! `
+    );
     body.append(text);
     setTimeout(() => {
       text.remove();
     }, 2000);
-    // console.log(`You did ${dmg} dmg to the boss`)
   }
   setTimeout(resetPlayer, 700);
-};
-
-
+  setTimeout(() => {
+    boss.attack();
+  }, 2000);
+}
--- src/utils/boss.js
+++ src/utils/boss.js
@@ -7,6 +7,20 @@ class Boss {
     this.hp = 3000,
     this.mp = 9999,
     this.dmg = 50
+    this.reset = this.reset.bind(this)
+  }
+
+  reset() {
+    this.x = 100
+  }
+
+  attack () {
+    this.x = 200
+    let hitChance = Math.round(Math.random() * 10);
+    if (hitChance >= 2) {
+      let dmg = Math.random(Math.random() * 10) + this.dmg;
+    }
+    setTimeout(this.reset, 700)
   }
 };
 
