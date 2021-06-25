0607884d1635312facc98fd6c51942bbe46554b3
jon <jonathanyusf@gmail.com>
2021-06-25 09:59:33 -0700

    Add placeholder characters

--- src/index.js
+++ src/index.js
@@ -15,6 +15,20 @@ const boss = new Boss();
 
 const player = new Player();
 
+const deadFrog = {
+  x: 700,
+  y: 300,
+  width: 50,
+  height: 28
+};
+
+const deadTerra = {
+  x: 700,
+  y: 380,
+  width: 48,
+  height: 32
+};
+
 const bossSprite = new Image();
 bossSprite.src = "./src/utils/assets/boss.gif";
 
@@ -22,16 +36,27 @@ const background = new Image();
 background.src = "./src/utils/assets/background.gif";
 
 const playerSprite = new Image();
-playerSprite.src = "./src/utils/assets/cecil.gif";
+playerSprite.src = "./src/utils/assets/Crono.gif";
+
+const deadSprite1 = new Image();
+deadSprite1.src = "./src/utils/assets/Frog.gif"
+
+const deadSprite2 = new Image();
+deadSprite2.src = "./src/utils/assets/Terra.gif"
 
 const attackAudio = new Audio();
 attackAudio.src = "./src/utils/assets/attack.mp3";
+
+const bossAttAudio = new Audio();
+bossAttAudio.src ="./src/utils/assets/boss.mp3"
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(bossSprite, boss.x, boss.y, boss.width, boss.height);
   ctx.drawImage(playerSprite, player.x, player.y, player.width, player.height);
+  ctx.drawImage(deadSprite1, deadFrog.x, deadFrog.y, deadFrog.width, deadFrog.height);
+  ctx.drawImage(deadSprite2, deadTerra.x, deadTerra.y, deadTerra.width, deadTerra.height);
   requestAnimationFrame(animate);
 }
 
@@ -63,5 +88,6 @@ function attack() {
   setTimeout(resetPlayer, 700);
   setTimeout(() => {
     boss.attack();
+    bossAttAudio.play();
   }, 2000);
 }
--- src/utils/player.js
+++ src/utils/player.js
@@ -2,8 +2,8 @@ class Player {
   constructor(props) {
     this.x = 700,
     this.y = 200,
-    this.width = 32,
-    this.height = 48,
+    this.width = 62,
+    this.height = 62,
     this.hp = 999,
     this.mp = 999,
     this.dmg = 50
